1a615731cc2fc67c5ea822c4648b5d353a9f6084
Herman TANG <347877478@qq.com>
2020-02-09 14:23:44 +0800

    Create inv_cnt.py

---
+++ Q1/inv_cnt.py
@@ -0,0 +1,9 @@
+def merge(left, right):
+    res = []
+    while left and right:
+        if left[0] < right[0]:
+            res.append(left.pop(0))
+        else:
+            res.append(right.pop(0))
+    res = res + left + right
+    return res
